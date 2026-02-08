c86b47f64a7c6b5ae9398e1167f0b9599b9a881e
“Berry” <info@felix-behrens.de>
2026-02-08 18:36:05 +0100

    Added better Layout

--- src/Tracker.js
+++ src/Tracker.js
@@ -9,6 +9,7 @@ export function Tracker({
 }) {
   const [sortFilter, setSortFiler] = useState("desc");
 
+  const isEmpty = monsters.length === 0;
   let sortedMonsters = monsters.sort((a, b) => b.init - a.init);
 
   if (sortFilter === "asc") {
@@ -27,23 +28,36 @@ export function Tracker({
       className="tracker-main"
       onChange={(e) => setSortFiler(e.target.value)}
     >
-      <select value={sortFilter} className="sort-filter">
-        <option value="desc">Descending</option>
-        <option value="asc">Ascending</option>
-        <option value="name-az">By name (A-Z)</option>
-        <option value="name-za">By name (Z-A)</option>
-      </select>
-      <div className="tracker-monster">
-        {sortedMonsters.map((monster) => (
-          <Monster
-            monster={monster}
-            onDeleteMonster={onDeleteMonster}
-            key={monster.id}
-            onChangeMonster={onChangeMonster}
-            onCalcHp={onCalcHp}
-          />
-        ))}
-      </div>
+      {!isEmpty ? (
+        <>
+          <select value={sortFilter} className="sort-filter">
+            <option value="desc">Descending</option>
+            <option value="asc">Ascending</option>
+            <option value="name-az">By name (A-Z)</option>
+            <option value="name-za">By name (Z-A)</option>
+          </select>
+          <div className="tracker-monster">
+            <div className="monster-row header">
+              <span></span>
+              <span>Name</span>
+              <span>Init</span>
+              <span>HP</span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </>
+      ) : null}
+      {sortedMonsters.map((monster) => (
+        <Monster
+          monster={monster}
+          onDeleteMonster={onDeleteMonster}
+          key={monster.id}
+          onChangeMonster={onChangeMonster}
+          onCalcHp={onCalcHp}
+        />
+      ))}
     </div>
   );
 }
